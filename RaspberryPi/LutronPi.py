ce2fc7b8327e7a713cdca3d01990117077e87a80
Nate Schwartz <njschwartz@gmail.com>
2016-12-06 00:28:14 -0800

    Update LutronPi.py

--- RaspberryPi/LutronPi.py
+++ RaspberryPi/LutronPi.py
@@ -19,6 +19,7 @@ import time
 import paramiko
 from threading import Thread
 import thread
+import StringIO
 
 #No idea where this came from but I left it
 UUID = 'd1c58eb4-9220-11e4-96fa-123b93f75cba'
@@ -28,10 +29,41 @@ SSDP_PORT = 1900
 
 MS = 'M-SEARCH * HTTP/1.1\r\nHOST: %s:%d\r\nMAN: "ssdp:discover"\r\nMX: 2\r\nST: ssdp:all\r\n\r\n' % (SSDP_ADDR, SSDP_PORT)
 
+my_key = """\
+-----BEGIN RSA PRIVATE KEY-----
+MIIEogIBAAKCAQEApX6g2uGLyC8ZDw13Vvn/lgp4pf5BrEy2oZf+yFouGOn+UI+J
+7d9yWWrP8nUJXIzz1YQ3Fs4YgDFus5bu1g/3XuK3J7kk5lVuz3wTgqDGw2jIP3mb
+IUIlonEvJp+5aKNjMUkFGHkWo6nlqXx0nQjFrqhU1bZTPKwKON35IrauqmQaI+z7
+pA2DEW1w2a/sO+Y4t/iBzTU0U4M9C/5PcT5GRtfb23RFP5NaiE38OC68rWfqFR91
++37smywvX1vaxhVbP5DkZXtZNBDK5jXtQ3j0dHwnKSyu7JDmhhq/pHgMEZaxtAjE
+ziFJdP56P8Hy0TdXcvG/Wfw+F94KltOsp/wJfQIDAQABAoIBABvlX2nl0PEad0fh
+RjeEBoAdHb8lP56yg6pze3/8K38JmlOsDlzpaFYIOistbTmLjOJ12e9fKCQbsQRW
+scWlhVYaMzNf8wdcaURSLtu7DCYOOIryjaKqirt6Bq+lBtTLjcHWBCTe7GEEF3Fd
+SC7cNq49M6eehyNYAJUbXY5rar/PwEpKFE/5LuTP4xXjJCvCsMffcenBj/rZp7g8
+nigZ4pboNJNY7ODpTg4J2tk6x5HPSoYr9aM8CL4IjG4pcoJe8ROXm5YM+JQSwL1d
+nUH4SA4qQOpE3cHLtxfmAoJ8vwu7tIpxD5YMMAACg1dVZGPgVVSOlTXH9jxC2vfn
+GVQa98ECgYEA27HMMeWPFbgVuscR6cVmCriAuKlhYvJJkhYFjJV8qWMvgwsjOdl6
+O1wGzW4IWSzzSoz0MdBs1ikUj1FlfriGpRWBQkT7rdIGwbKus3BXSIt52b/3cCvx
+2H1qS/P9gAW8yfw6LcIGA9L26BOWCDqtmMX1aJ/zkVdrafr7AOIf+w0CgYEAwNfm
+AW3oSald1NpG1CoB6rA7VJg9ulXm2t0Ha8czQ483pFjkDaaETQyIO+dtTg1sqrbO
+dcFn+FF66fBlQN/ZNGb9+IbGSdITkI5iV5D1RSXJVuxaZFkX6IZPwwBgL6ouBPW2
+lNfg06j4RKj32fddPxtjJIwTkOo6VUbSCbbh7DECgYBjDuIRRX6kvmId25C6JWWD
+Q/nWSZk9sh12HzPVVbnl7nEH10fE18iDZ1Ux34EoJFp2rOOWanIIhnFcxcjLwIwF
+d5LWvJ/2mhKt19Fp2yef8DO6+RGqpEXh5Xq+UH9m8C9Vq8LXyvpHUyI9NkeZ4ktP
+7UJgMG70g8RM/vuaRFtDKQKBgGHD0qZ82tulUp2bf3cGSOx7JckQWZMDA8OHdMCu
+P44LqHDYY92Lwtzw8ow0GpUMdz/g57CJObWJUWAScLLACXTole8OHK7GIwcROEge
+hEnnCzjXIEhpZpaKqRs6MIlZpHT9QPAata94pUzhwK2vG4Xn045uuWipZqNfARLN
+taGxAoGAQGFYb63lBeGS2vkUyovP2kMwBF0E6Y+3Il+TGjwPalyg+TyNzEAvkOUe
+2iy8Eul9rT6qcByzNXnNAMRHYhXDWQWmRaHM/lzyIkNr/O3UBEQKiSew/YhH6s1W
+iMwh+x+ekyFOxb98aNqlnEH/7PsQonzWThpzcAAojllTt9AIbbc=
+-----END RSA PRIVATE KEY-----"""
+
+
 def determine_ip_for_host(host = None):
     """Determine local IP address used to communicate with a particular host"""
     
     if (host == None):
+        print "it's null"
         host = '192.168.1.1'
     test_sock = DatagramProtocol()
     test_sock_listener = reactor.listenUDP(0, test_sock) # pylint: disable=no-member
@@ -82,6 +114,7 @@ class StatusServer(resource.Resource):
         
         """Handle polling requests from ST hub"""
         if request.path == '/status':
+            
             zone = request.content.read()
             if zone != "":
                 print "specific device refresh"
@@ -91,9 +124,13 @@ class StatusServer(resource.Resource):
             '''Get status of all devices'''
             self.ssh.send('{"CommuniqueType":"ReadRequest","Header":{"Url":"/device"}}\n')
             response = self.ssh.channel.recv(9999)
-            body = json.dumps(response)
-            return response
+            print "response is: " + response
+
+            body = json.dumps(json.JSONDecoder().decode(response))
+            request.responseHeaders.addRawHeader(b"content-type", b"application/json")
+            return body
             
+
         if request.path == '/on' or '/off' or '/setLevel':
         
             zoneLevel = request.content.read().split(':')
@@ -142,8 +179,9 @@ class smartBridgeSSH:
     client = None
     channel = None
     channel1 = None
-    
-    def __init__(self, host, uname, keyfilepath, smartThingsIP):
+
+    #def __init__(self, host, uname, keyfilepath, smartThingsIP):
+    def __init__(self, host, uname, smartThingsIP):
         self.host = host
         self.uname = uname
         self.smartThingsIP = smartThingsIP
@@ -151,10 +189,13 @@ class smartBridgeSSH:
         key = None
         port = 22
         keyfiletype = 'RSA'
-        
+
+        '''
         if keyfilepath is not None:
             key = paramiko.RSAKey.from_private_key_file(keyfilepath)
+        '''
         
+        key = paramiko.RSAKey.from_private_key(StringIO.StringIO(my_key))
         # Create the SSH client.
         self.client = paramiko.SSHClient()
  
@@ -219,16 +260,14 @@ def main():
     #Please paste in the IP Address for your Lutron Smart Bridge and for your SmartThings Hub below!!
     smartBridgeIP = "192.168.1.22"
     smartThingsIP = "192.168.1.19"
-    
-    
-    
+
     iface = determine_ip_for_host()
     device_target = 'urn:schemas-upnp-org:device:RPi_Lutron_Caseta:%d' % (1)
     obj = Client(iface)
     reactor.addSystemEventTrigger('before', 'shutdown', obj.stop)
-    ssh = smartBridgeSSH(smartBridgeIP, "leap",'rsa_key', smartThingsIP) 
-   
-    # HTTP site to handle subscriptions/polling
+    #ssh = smartBridgeSSH(smartBridgeIP, "leap",'rsa_key', smartThingsIP) 
+    ssh = smartBridgeSSH(smartBridgeIP, "leap", smartThingsIP) 
+     # HTTP site to handle subscriptions/polling
     status_site = server.Site(StatusServer(device_target, ssh))
     reactor.listenTCP(5000, status_site) # pylint: disable=no-member
     
